ba8463247f7596430ca948ac06018d74a650371d
sergey <sergey@sergey-notebook.(none)>
2013-03-10 18:34:57 +0300

    setup file

--- setup.py
+++ setup.py
@@ -1,1 +1,14 @@
+from distutils.core import setup
 
+setup(
+    name='Wiseweb Storage Client',
+    version='0.1.0',
+    author='Sergey Kotov',
+    author_email='ikoshak@gmail.com',
+    packages=['ww_storage_client',],
+    url='https://github.com/ikoshak/ww_storage_client.git',
+    license='',
+    description='Client lib for Wiseweb Storage',
+    long_description='',
+    install_requires=[],
+)
